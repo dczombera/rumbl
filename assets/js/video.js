04e2411bf8980399e39905e834576585db060070
Dennis Czombera <milchschaumschluerfer@gmail.com>
2017-07-18 15:22:59 +0200

    Add VideoChannel; Join topic on client site

--- assets/js/video.js
+++ assets/js/video.js
@@ -17,7 +17,12 @@ let Video = {
     let msgContainer = document.getElementById("msg-container")
     let msgInput     = document.getElementById("msg-input")
     let postButton   = document.getElementById("msg-submit")
-    let vidChannel   = socket.channel(`videos: ${videoId}`)
+    let vidChannel   = socket.channel(`videos:${videoId}`)
+    vidChannel.on("ping", ({count}) => console.log("PING", count))
+
+    vidChannel.join()
+      .receive("ok", resp => console.log("joined the video channel", resp))
+      .receive("error", reason => console.log("join failed", reason))
   }
 }
 
